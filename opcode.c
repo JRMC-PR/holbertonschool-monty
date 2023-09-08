f627a61897a05c3f7a6acd5353574314b51a2f21
GuilleP2018 <gpereyo@gmail.com>
2023-09-08 17:12:00 +0000

    current updates

--- opcode.c
+++ opcode.c
@@ -43,15 +43,57 @@ void push(m_stack_t **stack, unsigned int line_number)
  *@line_number: lines in input;
  */
 
-void pall(m_stack_t **stack, unsigned int line_number)
+void pall // print all
 {
-	/*declarations*/
-	m_stack_t *tmp = *stack;
-	/*void unused parameter*/
+	m_stack_t *current = *stack;
 	(void)line_number;
-	while (tmp)
+
+	while (current)
 	{
-		printf("%d\n", tmp->n);
-		tmp = tmp->next;
-	} /*end while*/
-} /*end function*/
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+}
+
+void pint //print current
+{
+	//if error
+	//else
+	printf("%d\n", (*stack)->n);
+}
+
+void pop //remove from stack
+{
+	//if error
+	stack_t *tmp = *stack;
+	*stack = (*stack)->next;
+
+	if (*stack)
+		(*stack)->prev = NULL;
+
+	free(tmp);
+
+}
+
+void swap //swap top 2 elements of stack
+{
+	//if for errors
+
+	int tmp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = tmp;
+
+}
+
+void add // add top 2 elements of stack
+{
+	//if for errors
+	(*stack)->next->n += (*stack)->n;
+	//send to pop
+}
+
+void nop
+{
+ //void everything given to func
+}
+
