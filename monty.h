c492de8906924861ed02e4b1caa8a0c2c236d005
GuilleP2018 <gpereyo@gmail.com>
2023-09-22 17:58:11 +0000

    betty fixes

--- monty.h
+++ monty.h
@@ -8,6 +8,15 @@
 #include <ctype.h>
 #include <stdbool.h>
 
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
 typedef struct m_stack_s
 {
     int n;
@@ -15,6 +24,14 @@ typedef struct m_stack_s
     struct m_stack_s *next;
 } m_stack_t;
 
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
 typedef struct instruction_s
 {
     char *opcode;
